874c43a29752b3fd5beafcfea70a119a4e17aa15
DeepLearning VM <idanbasre@gmail.com>
2020-03-13 07:49:26 +0000

    Deleted tranier dir.

--- run.sh
+++ run.sh
@@ -1,16 +1,11 @@
-#!bin/bash
+#!/bin/bash
 
 
 TRAIN_DATA_DIR=UCIData
 LOGS_DIR=Logs
-LAYER_WIDTH=128,1
+LAYER_WIDTH=128,10,10,1
 P=2
 NUM_EPOCHS=10
 LR=0.001
 
-python -m trainer.task --train_data_dir $TRAIN_DATA_DIR \\
- 		--logs_dir $LOGS_DIR \\
- 		--layer_width $LAYER_WIDTH \\
-        --p $P \\
-        --num_epochs $NUM_EPOCHS \\
-        --lr $LR
+python -m trainer.task --train_data_dir $TRAIN_DATA_DIR --logs_dir $LOGS_DIR --layer_width $LAYER_WIDTH --p $P --num_epochs $NUM_EPOCHS --lr $LR
